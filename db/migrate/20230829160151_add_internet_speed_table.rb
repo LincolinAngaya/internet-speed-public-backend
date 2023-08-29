c6fd1a623c4160b3a7bd0236080c735a8a72b12f
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-08-29 19:22:09 +0300

    migrate the internetspeed migrate

--- db/migrate/20230829160151_add_internet_speed_table.rb
+++ db/migrate/20230829160151_add_internet_speed_table.rb
@@ -1,11 +1,14 @@
 class AddInternetSpeedTable < ActiveRecord::Migration[7.0]
   def change
     create_table :internet_speeds, id: :uuid do |t|
-      t.references :place, null: false, foreign_key: true
-      t.float :download_speed, null: false, scale: 2
-      t.string :download_units, null: false
-      t.string :city, null: false
 
+      t.references :place, type: :uuid, null: false, foreign_key: true
+      # t.references :place, null: false, foreign_key: true
+      # t.decimal :download_speed, null: false, scale: 2
+      t.decimal :download_speed, precision: 10, scale: 2, null: false
+
+      t.string :download_units, null: false
+    
       t.timestamps
     end
   end
