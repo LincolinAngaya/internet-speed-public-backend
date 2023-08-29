1cf282140cbdfcad6fe23ad7364816ecd58e00ab
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-08-29 22:41:25 +0300

    done adding endpoints

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -12,5 +12,20 @@ module Api
               }
         end
     end
+
+    def most_recent_download_speed(place)
+        # Assume that all the units are the same.
+        place.internet_speeds.order("created_at").last&.download_speed
+    end
+  
+    def most_recent_download_units(place)
+        place.internet_speeds.order("created_at").last&.download_units
+    end
+  
+    def number_of_measurements(place)
+        place.internet_speeds.count
+    end
+    
+  
     end
 end
