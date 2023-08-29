dfb2e2f1b4967249dc08a8ab786f950d8d23ec05
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-08-29 21:30:41 +0300

    create new react component called placelist

---
+++ app/javascript/components/places_list.jsx
@@ -0,0 +1,10 @@
+import {react} from "react";
+import { ReactDom } from "react-dom";
+class PlaceList extends React.Component{
+    render(){
+        return(
+            <div>places list rendered in react</div>
+        )
+        
+    }
+}
