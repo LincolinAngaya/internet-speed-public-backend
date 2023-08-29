61da04f65e575b83e1db145c770505ce666cba89
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-08-29 19:11:23 +0300

    add internetspeed parameters and add a few validatios

--- db/migrate/20230829160151_add_internet_speed_table.rb
+++ db/migrate/20230829160151_add_internet_speed_table.rb
@@ -1,4 +1,12 @@
 class AddInternetSpeedTable < ActiveRecord::Migration[7.0]
   def change
+    create_table :internet_speeds, id: :uuid do |t|
+      t.references :place, null: false, foreign_key: true
+      t.float :download_speed, null: false, scale: 2
+      t.string :download_units, null: false
+      t.string :city, null: false
+
+      t.timestamps
+    end
   end
 end
