343c29276fffb1168c4fa70e38007e88d031fb0f
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-08-29 21:38:47 +0300

    correct the class name

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,6 +1,6 @@
 import {react} from "react";
 import { ReactDom } from "react-dom";
-class PlaceList extends React.Component{
+class PlacesList extends React.Component{
     render(){
         return(
             <div>places list rendered in react</div>
