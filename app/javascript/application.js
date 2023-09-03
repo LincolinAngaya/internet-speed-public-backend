a3ca6647aa58598f5938424e49feff1d4f874307
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 23:05:43 +0300

    failed to render react pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
-// Entry point for the build script in your package.json
-// Entry point for the build script in your package.json
+
 import "./components/places_list"
--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,14 +1,13 @@
-import {React} from "react"
-import { ReactDOM } from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-class PlaceList extends React.Component{
-  render()
-  {
-    return(
+class PlaceList extends React.Component {
+  render() {
+    return (
       <div>place list rendered</div>
-    )
+    );
   }
 }
 
 const placesList = ReactDOM.createRoot(document.getElementById("places-list-container"));
-placesList.render(<  PlaceList/>);
+placesList.render(<PlaceList />);
