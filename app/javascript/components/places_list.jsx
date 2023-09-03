10d7f59a08ee556356936a8f5181e7f404cf8508
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-03 22:41:23 +0300

    its rendering correctly

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,14 +1,14 @@
-import react from "react";
-import ReactDom  from "react-dom/client";
-class PlacesList extends React.Component{
-    render(){
-        return(
-            <div>places list rendered in react</div>
-        )
-        
-    }
+import {React} from "react"
+import { ReactDOM } from "react-dom"
+
+class PlaceList extends React.Component{
+  render()
+  {
+    return(
+      <div>place list rendered</div>
+    )
+  }
 }
 
-
 const placesList = ReactDOM.createRoot(document.getElementById("places-list-container"));
-placesList.render(<PlacesList />);
+placesList.render(<  PlaceList/>);
